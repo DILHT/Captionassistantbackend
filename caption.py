593e73c65e9e9bac99da4b0789633e8505fa0ebc
Daniel Kasambala <danielkasambala51@gmail.com>
2025-07-18 01:41:58 +0200

    frontend linked

--- caption.py
+++ caption.py
@@ -15,7 +15,7 @@ client = OpenAI(
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://captionassistant.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
